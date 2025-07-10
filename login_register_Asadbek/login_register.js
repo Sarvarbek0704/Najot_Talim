96ae6cc2c28723513a6d33c429ce522e261590e4
Asadbek5511 <ochilovasadbek278@gmail.com>
2025-07-11 00:31:53 +0500

    registratsiya

---
+++ login_register_Asadbek/login_register.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const signUpBtn = document.querySelector('.register-form .btn-primary');
+
+  signUpBtn.addEventListener('click', () => {
+    const name = document.querySelector('input[placeholder="Name"]').value.trim();
+    const email = document.querySelector('input[placeholder="Email"]').value.trim();
+    const password = document.querySelector('input[placeholder="Password"]').value.trim();
+    const confirm = document.querySelector('input[placeholder="Confirm password"]').value.trim();
+
+    if (!name || !email || !password || !confirm) {
+      alert('Barcha maydonlarni toldiring.');
+      return;
+    }
+
+    if (password !== confirm) {
+      alert('Parollar mos emas!');
+      return;
+    }
+
+    let users = JSON.parse(localStorage.getItem('users')) || [];
+
+    const userExists = users.some(user => user.email === email);
+
+    if (userExists) {
+      alert("Bu foydalanuvchi allaqachon mavjud!");
+      return;
+    }
+
+    users.push({ name, email });
+    localStorage.setItem('users', JSON.stringify(users));
+
+    alert("Royxatdan otish muvaffaqiyatli!");
+    window.location.href = "./users.html";
+  });
+});
